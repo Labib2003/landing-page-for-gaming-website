b71b80924ecd721c9b0648b02d30e330ed00d237
Labib2003 <labib.salimi30@gmail.com>
2022-07-22 15:47:31 +0600

    wornings resolved

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,26 +10,26 @@ const Navbar = () => {
     </>
 
     return (
-        <div class="navbar bg-white bg-opacity-20 p-0 border-b-white border-b">
-            <div class="dropdown">
-                <label tabindex="0" class="btn btn-ghost lg:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        <div className="navbar bg-white bg-opacity-20 p-0 border-b-white border-b">
+            <div className="dropdown">
+                <label tabIndex="0" className="btn btn-ghost lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                 </label>
-                <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                     {navbarLinks}
                 </ul>
             </div>
-            <div class="hidden lg:flex w-full">
-                <ul class="menu menu-horizontal p-0 saira uppercase font-bold text-lg text-white w-full justify-around">
+            <div className="hidden lg:flex w-full">
+                <ul className="menu menu-horizontal p-0 saira uppercase font-bold text-lg text-white w-full justify-around">
                     {navbarLinks}
                     <li>
-                        <div class="form-control border-white">
-                            <input type="text" placeholder="Search for Tournaments,games etc"  className='input'/>
+                        <div className="form-control border-white">
+                            <input type="text" placeholder="Search for Tournaments,games etc" className='input' />
                         </div>
                     </li>
                 </ul>
             </div>
-            <div className='navbar-end mr-3'>
+            <div className='navbar-end ml-auto lg:ml-0 mr-3'>
                 <button className='custom-button bg-white'><p className='saira px-24 py-4 uppercase'>Login</p></button>
                 <button className='custom-button'><p className='saira px-24 py-4 text-white uppercase'>Sign Up</p></button>
             </div>
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,20 +4,24 @@ import Navbar from '../components/Navbar';
 const LandingPage = () => {
     return (
         <div>
-            <div class="hero min-h-screen mb-[41px]" style={{ backgroundImage: "url(" + "images/Untitled-1.jpg" + ")" }}>
+            <div className="hero min-h-screen mb-[41px]"
+                style={{ backgroundImage: "url(" + "images/Untitled-1.jpg" + ")" }}
+            >
                 <div className='absolute top-0 w-full'>
                     <Navbar />
                 </div>
-                <div class="hero-overlay bg-opacity-60"></div>
-                <div class="hero-content text-center text-white">
-                    <div class="uppercase">
-                        <h1 class="max-w-[50rem] mx-auto mb-7 text-7xl font-bold leading-normal tracking-wide">Evolve Your Gaming Experience</h1>
-                        <p class="max-w-4xl mx-auto mb-16 text-2xl font-bold">Play with friends or compete against rivals from around the world</p>
-                        <div className='bg-white text-black text-lg w-96 mx-auto px-32 py-6 rounded'>Join Now</div>
+                <div className="hero-overlay bg-opacity-60 poppins"></div>
+                <div className="hero-content text-center text-white">
+                    <div className="uppercase">
+                        <h1 className="max-w-[50rem] mx-auto mb-7 text-7xl font-bold leading-normal tracking-wide poppins"
+                            style={{ "textShadow": "10px 10px 0px #000000" }}
+                        >Evolve Your Gaming Experience</h1>
+                        <p className="max-w-4xl mx-auto mb-16 text-2xl font-bold poppins leading-normal tracking-wide">Play with friends or compete against rivals from around the world</p>
+                        <div className='bg-white text-black text-lg w-96 mx-auto px-32 py-6 rounded poppins'>Join Now</div>
                     </div>
                 </div>
             </div>
-            <div className='grid grid-cols-5 mb-40 max-w-screen-2xl mx-auto'>
+            <div className='grid grid-cols-5 gap-3 mb-40 max-w-screen-2xl mx-auto'>
                 <div className='w-[139px] h-[139px] rounded-full bg-blue-500 border-[18px] border-blue-400 grid place-content-center mx-auto'>
                     <img src="images/icon1.png" alt="icon" />
                 </div>
@@ -29,17 +33,17 @@ const LandingPage = () => {
                 <div className='w-[139px] h-[139px] rounded-full bg-blue-500 border-[18px] border-blue-400 grid place-content-center mx-auto'>
                     <img src="images/icon3.png" alt="icon" />
                 </div>
-                <div className='mx-auto text-center'>
+                <div className='mx-auto text-center poppins'>
                     <h1 className='font-semibold text-2xl uppercase'>Sign Up</h1>
                     <p>Create your account</p>
                 </div>
                 <div></div>
-                <div className='mx-auto text-center'>
+                <div className='mx-auto text-center poppins'>
                     <h1 className='font-semibold text-2xl uppercase'>Compete</h1>
                     <p>Join Tournaments to compete with others</p>
                 </div>
                 <div></div>
-                <div className='mx-auto text-center'>
+                <div className='mx-auto text-center poppins'>
                     <h1 className='font-semibold text-2xl uppercase'>Win & earn</h1>
                     <p>Win and earn in the form of crypto</p>
                 </div>
